5804061e6ac856293e9a2d91602c735e3f28fc3d
Shrey97 <shreytanna97@gmail.com>
2017-10-05 11:16:27 +0000

    Update settings.py

--- attendance_2/settings.py
+++ attendance_2/settings.py
@@ -135,12 +135,6 @@ MEDIA_ROOT = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
